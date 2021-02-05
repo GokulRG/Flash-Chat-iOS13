6953a0a66181a285e8246734df4ca9e27d27f876
GokulRG <ramakrishnan.go@husky.neu.edu>
2021-02-05 00:01:02 -0800

    Working TableView and DataSource

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,7 +14,7 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
-    var messages = [
+    var messages: [Message] = [
         Message(sender: "kanewilliamson@mailinator.com", body: "Hey"),
         Message(sender: "dwarner@mailinator.com", body: "Yo"),
         Message(sender: "kanewilliamson@mailinator.com", body: "Wazzzupppp!")
@@ -22,6 +22,7 @@ class ChatViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         title = K.appName
         navigationItem.hidesBackButton = true
     }
@@ -42,6 +43,7 @@ class ChatViewController: UIViewController {
     }
 }
 
+//MARK: - UITableViewDataSource Extension
 extension ChatViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         messages.count
@@ -49,9 +51,8 @@ extension ChatViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
-        cell.textLabel?.text = "Hey there cell"
+        cell.textLabel?.text = messages[indexPath.row].body
         return cell
     }
-    
-    
 }
+
